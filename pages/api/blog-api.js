14f907ed19392f99a52c3edbdb45ec9043b8bb6d
Lucas Aho <aholucascode@gmail.com>
2021-04-15 11:07:07 -0600

    Added login/logout routes

--- pages/api/blog-api.js
+++ pages/api/blog-api.js
@@ -3,14 +3,23 @@ import axios from 'axios';
 const queryURL = "https://guarded-bastion-16379.herokuapp.com/blog/" 
 
 const API = {
+    //Article Routes
     getAllPosts: function() {
         return axios.get(queryURL + "find/all");
     },
     getPostById: function(id) {
         return axios.get(queryURL + "find/" + id);
     },
-    getMostRecent: function() {
-        return axios.get(queryURL + "find/recent");
+
+    //User Routes
+    newUser: function (userData) {
+        return axios.post("/api/users", userData);
+    },
+    login: function (userData) {
+        return axios.post("/api/login", userData);
+    },
+    logout: function() {
+        return axios.get("/logout");
     }
     
 }
